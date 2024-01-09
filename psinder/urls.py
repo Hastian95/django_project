b3327cafa212838dc96810b6bc3486c5c0b21c5c
Pawel <hastia95@gmail.com>
2024-01-09 22:37:36 +0100

    8 commit - Add Create and Update form of posts

--- psinder/urls.py
+++ psinder/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
@@ -9,6 +9,8 @@ from users import views as user_views
 urlpatterns = [
     path('', views.home, name='psinder-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('about/', PostListView.as_view(), name='psinder-about'),
     path('manyviews/', views.manyviews, name='psinder-manyviews'),
 ]
